2f34809693c46ea794fb2ad2cab4b86f1b2e015f
sunmix <sunmix@users.noreply.github.com>
2016-04-14 09:35:37 +0800

    Add trash

---
+++ trash.rb
@@ -0,0 +1,14 @@
+class Trash < Formula
+  desc "Small command-line program that asks Finder to move files or folders to the trash"
+  homepage "https://github.com/sunmix/trash"
+  url "https://github.com/sunmix/trash/archive/v0.1.0.tar.gz"
+  sha256 "486af79999f62a8bb6d35a47cfbb08675801dc4026a4009a34c630f11cab4432"
+
+  depends_on :xcode => :build
+  depends_on :macos => :el_capitan
+
+  def install
+    xcodebuild "-target", "trash", "-configuration", "Release", "SYMROOT=symroot", "OBJROOT=objroot"
+    bin.install "symroot/Release/trash"
+  end
+end
