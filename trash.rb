a0f8d4021878db13be7916561968537f0340a30d
sunmix <sunmix@users.noreply.github.com>
2016-04-14 10:10:35 +0800

    trash: conflicts_with "osxutils"

--- trash.rb
+++ trash.rb
@@ -4,6 +4,8 @@ class Trash < Formula
   url "https://github.com/sunmix/trash/archive/v0.1.0.tar.gz"
   sha256 "486af79999f62a8bb6d35a47cfbb08675801dc4026a4009a34c630f11cab4432"
 
+  conflicts_with "osxutils", :because => "both install a trash binary"
+
   depends_on :xcode => :build
   depends_on :macos => :el_capitan
 
